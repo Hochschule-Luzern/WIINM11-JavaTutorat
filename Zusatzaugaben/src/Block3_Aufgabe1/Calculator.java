0d845d3c085a742f38191e1b7cbe47dcd42901c5
Janik von Rotz <login@janikvonrotz.ch>
2015-10-05 11:19:34 +0200

    update

--- Zusatzaugaben/src/Block3_Aufgabe1/Calculator.java
+++ Zusatzaugaben/src/Block3_Aufgabe1/Calculator.java
@@ -1,13 +1,59 @@
 package Block3_Aufgabe1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 	
-	public isPrim(){
+	public static void main(String[] args){
+		int zahl1 = 17;
+		int zahl2 = 24;
 		
+		if(isPrim(zahl1)){
+			
+			System.out.println("Zahl: " + zahl1 + " ist eine Primzahl.");
+		}else{
+			
+			System.out.println("Keine Primzahl");
+			System.out.println("Primfaktoren: ");
+			List<Integer> list = primeFactors(zahl1);
+			for(Integer i : list){
+				System.out.println(i);
+			}
+		}
+		
+		if(isPrim(zahl2)){
+			
+			System.out.println("Zahl: " + zahl2 + " ist eine Primzahl.");
+		}else{
+			System.out.println("Keine Primzahl");
+			System.out.println("Primfaktoren: ");
+			List<Integer> list = primeFactors(zahl2);
+			for(Integer i : list){
+				System.out.println(i);
+			}
+		}
 	}
 	
-	public primFaktoren(){
+	public static boolean isPrim(int zahl){
 		
+		for(int i=2; i<zahl; i++){
+           if(zahl%i == 0){
+               return false; //number is divisible so its not prime
+           }
+        }
+        return true; //number is prime now
 	}
+	
+	public static List<Integer> primeFactors(int zahl) {
+	    List<Integer> factors = new ArrayList<Integer>();
+	    for (int i = 2; i <= zahl; i++) {
+	      while (zahl % i == 0) {
+	        factors.add(i);
+	        zahl /= i;
+	      }
+	    }
+	    return factors;
+	  }
 	
 }
