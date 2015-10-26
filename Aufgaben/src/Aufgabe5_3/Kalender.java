323af7dd66b8f67d155347b68ceb97204b3fe00d
Janik von Rotz <login@janikvonrotz.ch>
2015-10-26 09:12:04 +0100

    update

---
+++ Aufgaben/src/Aufgabe5_3/Kalender.java
@@ -0,0 +1,20 @@
+package Aufgabe5_3;
+
+public class Kalender {
+	
+	public static void main(String args[]){
+		
+		int[][][][] kalender = new int[9][][][];
+		
+		for(int j=0; j<kalender.length;j++){
+			
+			for(int m=0;m<kalender[j].length;m++){
+				
+			}
+			
+		}
+		
+		
+	}
+
+}
--- Week6.md
+++ Week6.md
@@ -1,3 +1,14 @@
+# Review
+
+* Abstrakte Klassen/ Methoden
+* Interfaces
+* static, final, public, private
+  * Zugriffsrechte S.276
+
 # Repetition
 
-* Aufgabe 5.3
+* Aufgabe 5.3 Multidimensionale Arrays
+
+# Stack and Heap
+
+![](/images/stack_heap.jpg)
